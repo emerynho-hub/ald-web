c1f248baa38901641195d25e11cf575c3d06d855
CorieVie Dev <dev@corievie.app>
2026-06-26 17:29:11 +0000

    deploy v0.11.49 (auto depuis release.yml) emerynho-hub/corievie@41f0ec5434cc8e5da1c5e0f2c636d92ff98871d9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "910430234" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1373415683" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
